a059dbbc1af6167dcf8dd93e83c663e6311f1032
taylor west <13taylorwest37@gmail.com>
2019-03-14 16:06:17 -0400

    small changes made, kinda functional grid code added.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function makeDivs() {
-    let row = 5
-    for(let b = 0; b < row; b++){
-        for(let b = 0; b < row; b++){
-            let box = document.createElement("div");
-            box.style.margin("1px");
-            box.style.backgroundColor("black");
-            
-        }
+let gridRow = 20;
+let grid = gridRow * gridRow;
+let sketchBox = document.querySelector('.sketch-box')
+
+function makeGrid(gridRow, grid) {
+    for(let b = 0; b < grid; b++){
+        let div = document.createElement('div');
+        div.setAttribute('id', 'gridBox');
+        sketchBox.appendChild(div);
     }
+    sketchBox.setAttribute('style', 'grid: repeat(' + gridRow + ',auto) / repeat(' + gridRow + ',auto)');
 }
