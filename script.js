f7d439baebc700fae20c1505e9bd9cd59c4193c1
taylor west <13taylorwest37@gmail.com>
2019-03-14 14:39:09 -0400

    Created base files with html/css template for app. Began work on grid function.

---
+++ script.js
@@ -0,0 +1,11 @@
+function makeDivs() {
+    let row = 5
+    for(let b = 0; b < row; b++){
+        for(let b = 0; b < row; b++){
+            let box = document.createElement("div");
+            box.style.margin("1px");
+            box.style.backgroundColor("black");
+            
+        }
+    }
+}
