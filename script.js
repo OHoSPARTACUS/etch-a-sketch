c9976582c4c32bdd68e1eca55efee521ffc72be6
taylor west <13taylorwest37@gmail.com>
2019-03-18 11:31:11 -0400

    created working color changing function. Currently too basic, but operational!

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ let sketchBox = document.querySelector('.sketch-box');
 
 function makeDivs() {
     for (i = 0; i < divGrid; i++) {
-        let div = document.createElement('div');
+        let div = document.createElement('div')
         div.classList.add("grid-box");
         sketchBox.appendChild(div);
     }
 }
 
+function changeColor() {
+    let divs = document.querySelectorAll(".sketch-box div");
+    divs.forEach((div) => {
+        div.addEventListener("mouseover", function(e) {
+            e.target.setAttribute("style", "background: black");
+        });
+    });
+}
+
 makeDivs();
+changeColor();
