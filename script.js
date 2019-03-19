72e1a126687a02838e8e271374c8fcfeb56676ad
taylor west <13taylorwest37@gmail.com>
2019-03-19 07:44:20 -0400

    clear button function added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let divRow = 16;
 let divGrid = divRow * divRow;
 let sketchBox = document.querySelector('.sketch-box');
+let clearBtn = document.querySelector('.clear');
+let rainbowBtn =document.querySelector(".rainbow");
 
 function makeDivs() {
     for (i = 0; i < divGrid; i++) {
@@ -16,6 +18,13 @@ function changeColor() {
         div.addEventListener("mouseover", function(e) {
             e.target.setAttribute("style", "background: black");
         });
+    });
+}
+
+function clearGrid() {
+    let divs = document.querySelectorAll(".sketch-box div");
+    divs.forEach((div) => {
+        div.setAttribute("style", "background: white");
     });
 }
 
