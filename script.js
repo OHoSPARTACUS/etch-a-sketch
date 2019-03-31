bbdc9f42e64f9e9e9146123ba849665416b684f2
taylor west <13taylorwest37@gmail.com>
2019-03-31 10:42:06 -0400

    no real progress, saved most current changes

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+
+////////// VARIABLES //////////
+
 let divRow = 16;
 let divGrid = divRow * divRow;
+divSize = 495 / divRow - 1;
 let sketchBox = document.querySelector('.sketch-box');
 let clearBtn = document.querySelector('.clear');
 let rainbowBtn =document.querySelector(".rainbow");
 
+////////// FUNCTIONS //////////
+
 function makeDivs() {
     for (i = 0; i < divGrid; i++) {
-        let div = document.createElement('div')
+        let div = document.createElement('div');
         div.classList.add("grid-box");
         sketchBox.appendChild(div);
     }
+    setDivSize();
+}
+
+function setDivSize() {
+    let divs = document.querySelectorAll(".sketch-box divs");
+    divs.forEach((div) => {
+        div.setAttribute("style", "height: 20px");
+        div.setAttribute("style", "width: 20px");
+    });
 }
 
 function changeColor() {
@@ -25,11 +40,19 @@ function clearGrid() {
     while (sketchBox.firstChild) {
         sketchBox.removeChild(sketchBox.firstChild);
     }
+    newDivs();
 }
 
-function randomColor() {
-    
+function newDivs(num) {
+    num = prompt("Enter a number: 1 - 100");
+    divRow = num;
+    makeDivs();
+    changeColor();
 }
+
+////////// INITIAL LOAD //////////
 
 makeDivs();
 changeColor();
+console.log(divSize);
+console.log
